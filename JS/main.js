5b776dd7dcb561f07c3b81180b73183fd7e93c6e
stan <2314980309@qq.com>
2017-06-27 16:03:07 +0800

    功能已实现

--- JS/main.js
+++ JS/main.js
@@ -1,20 +1,27 @@
 
 var sentBtn=document.getElementById('sent')
 var more=document.getElementById('more');
-var script=document.createElement('script');
-var head = document.head
-head.appendChild(script)
+
 
 function getJSON(url) {
   script.src = url
 }
 
-function infoUpdating(json) {
+
+/*
+ *使用本地添加script标签的方法来请求
+ var script=document.createElement('script');
+ var head = document.head
+ head.appendChild(script)
+  function infoUpdating(json) {
   //test
-   var result = json.result
-  console.log(result)
-  
+    var result = json.result
+   console.log(result)
 }
+*
+*/
+
+
 function userinput () {
 	more.style.display="none";
 	sentBtn.style.display="block"
@@ -45,6 +52,7 @@ function addBotMes(value){
 	var bot_pic=document.createElement('i')
 	bot_pic.className='bot_pic'
 	bot_message.className='message'
+	bot_message.style.background='white'
 	bot_message.innerHTML=value
 	
 	botMe.appendChild(bot_pic)
@@ -60,8 +68,30 @@ function sentmessage(){
 	console.log('1')
 	if (sent_value != undefined) {
 	addUserMes(sent_value)}
-	addBotMes("哈哈哈")
-  	var url = 'http://www.tuling123.com/openapi/api?&info='+ sent_value +'&dtype=json&key=82fb88e4e494415da7de531fbdb6bd8c&callback=infoUpdating'
-   getJSON(url);
+	request.open('GET','http://www.tuling123.com/openapi/api?&info='+ sent_value +'&dtype=json&key=82fb88e4e494415da7de531fbdb6bd8c')
+	request.send();
+  //	var url = 'http://www.tuling123.com/openapi/api?&info='+ sent_value +'&dtype=json&key=82fb88e4e494415da7de531fbdb6bd8c&callback=infoUpdating'
+   //getJSON(url);
    input_box.value=' '
 }
+
+function success (text){
+	console.log(text);
+	var result=JSON.parse(text);
+	message=result.text
+	console.log(message)
+	console.log(typeof(text))
+	addBotMes(message)
+}
+function fail (text) {
+	console.log('返回数据失败')
+}
+var request = new  XMLHttpRequest();
+request.onreadystatechange =function(){
+	if(request.readyState==4){
+		if(request.status==200){
+			return success (request.responseText);
+		}else{
+			return fali(request.status)
+		}
+}}
